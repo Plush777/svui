707f8f47bc9d2d06800228f698786cdeac19ba73
Plush777 <sky11916@naver.com>
2025-01-21 09:53:11 +0900

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		edge: false,
+		split: true
+	}),
 
 	kit: {
 		adapter: adapter(),
